8e3bb0078359677bbf7727da7b1988ec152641ee
xanhday2 <anhnguyeenx2406@gmail.com>
2025-02-17 11:25:17 +0700

    bai4-17/2

---
+++ music.js
@@ -0,0 +1,24 @@
+let trackList = [
+    {
+        name:"cuoi nhau di",
+        path:"./CuoiNhauDiYesIDo-BuiAnhTuanHienHo-5947059.mp3",
+        img:"./anh1.jpg",
+        singer:"Bensound.com",
+    },
+    {
+        name:"Di giua troi ruc ro",
+        path:"./DiGiuaTroiRucRoAcousticVersion-NgoLanHuong-16559654.mp3",
+        img:"./anh2.jpg",
+        singer:"Bensound.com",
+    }, {
+        name:"mat ket noi",
+        path:"./MatKetNoi-DuongDomic-16783113.mp3",
+        img:"./anh3.jpg",
+        singer:"Bensound.com",
+    }, {
+        name:"yeu thi yeu khong yeu thi yeu",
+        path:"./YeuThiYeuKhongYeuThiYeu-AMee-6307375.mp3",
+        img:"./anh4.jpg",
+        singer:"Bensound.com",
+    },
+]
