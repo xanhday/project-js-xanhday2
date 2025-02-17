6339e79e2186768ffa8bcbac47c123cc466c0afb
xanhday2 <anhnguyeenx2406@gmail.com>
2025-02-17 10:53:20 +0700

    bai3-music,17-2

---
+++ script.js
@@ -0,0 +1,24 @@
+const play = document.querySelector(".play"),
+  previous = document.querySelector(".prev"),
+  next = document.querySelector(".next"),
+  //
+  trackImage = document.querySelector(".track-image"),
+  title = document.querySelector(".title"),
+  artist = document.querySelector(".artist"),
+  //
+  trackCurrentTime = document.querySelector(".current-time"),
+  trackDuration = document.querySelector(".duration-time"),
+  slider = document.querySelector(".duration-slider"),
+  //
+  showVolume = document.querySelector("#show-volume"),
+  volumeIcon = document.querySelector("#volume-icon"),
+  currentVolume = document.querySelector("#volume"),
+  //
+  autoPlayBtn = document.querySelector(".play-all"),
+  //
+  hamBurger = document.querySelector(".fa-bars"),
+  closeIcon = document.querySelector(".fa-times"),
+  //
+  musicPlaylist = document.querySelector(".music-playlist"),
+  pDiv = document.querySelector(".playlist-div"),
+  playList = document.querySelector(".playlist");
