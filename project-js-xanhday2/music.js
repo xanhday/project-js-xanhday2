ad7317c9c58044c204b5dd1cc0c09152fc84a0d3
xanhday2 <anhnguyeenx2406@gmail.com>
2025-02-19 11:21:11 +0700

    19/2/2025

---
+++ project-js-xanhday2/music.js
@@ -0,0 +1,24 @@
+let trackList = [
+    {
+        name:"cuoi nhau di",
+        path:"./music/CuoiNhauDiYesIDo-BuiAnhTuanHienHo-5947059.mp3",
+        img:"./images/anh1.jpg",
+        singer:"Bensound.com",
+    },
+    {
+        name:"Di giua troi ruc ro",
+        path:"./music/DiGiuaTroiRucRoAcousticVersion-NgoLanHuong-16559654.mp3",
+        img:"./images/anh2.jpg",
+        singer:"Bensound.com",
+    }, {
+        name:"mat ket noi",
+        path:"./music/MatKetNoi-DuongDomic-16783113.mp3",
+        img:"./images/anh3.jpg",
+        singer:"Bensound.com",
+    }, {
+        name:"yeu thi yeu khong yeu thi yeu",
+        path:"./music/YeuThiYeuKhongYeuThiYeu-AMee-6307375.mp3",
+        img:"./images/anh4.jpg",
+        singer:"Bensound.com",
+    },
+]
---
+++ project-js-xanhday2/script.js
@@ -0,0 +1,64 @@
+const play = document.querySelector(".play"),
+  previous = document.querySelector(".prev"),
+  next = document.querySelector(".next"),
+  //
+  trackImage = document.querySelector(".track-image"),
+  title = document.querySelector(".title"),
+  artist = document.querySelector(".artist"),
+  //
+  trackCurrentTime = document.querySelector(".current-time"),
+  trackDuration = document.querySelector(".duration-time"),
+  slider = document.querySelector(".duration-slider"),
+  //
+  showVolume = document.querySelector("#show-volume"),
+  volumeIcon = document.querySelector("#volume-icon"),
+  currentVolume = document.querySelector("#volume"),
+  //
+  autoPlayBtn = document.querySelector(".play-all"),
+  //
+  hamBurger = document.querySelector(".fa-bars"),
+  closeIcon = document.querySelector(".fa-times"),
+  //
+  musicPlaylist = document.querySelector(".music-playlist"),
+  pDiv = document.querySelector(".playlist-div"),
+  playList = document.querySelector(".playlist");
+
+  let timer;
+  let autoplay = 0;
+  let indexTrack = 0;
+  let songIsPlaying = false;
+  let track = document.createElement("audio");
+
+  // All Event Listeners
+  play.addEventListener("click", justPlay);
+
+  // Load Tracks
+  function loadTrack(indexTrack) {
+    track.src = trackList[indexTrack].path;
+    trackImage.src = trackList[indexTrack].img;
+    title.innerHTML = trackList[indexTrack].name;
+    artist.innerHTML = trackList[indexTrack].singer;
+    track.load();
+  }
+  loadTrack (indexTrack);
+  // Play Song or Pause Song
+  function justPlay() {
+    if (songIsPlaying == false) {
+      playSong();
+    } else
+    pauseSong();
+  } 
+
+  // Play Song
+  function playSong() {
+    track.play();
+    songIsPlaying = true;
+    play.innerHTML = '<i class="fas fa-pause"></i>';
+  }
+  // Pause Song
+  function pauseSong() {
+    track.pause();
+    songIsPlaying = false;
+    play.innerHTML = '<i class="fas fa-play"></i>';
+  }
+
